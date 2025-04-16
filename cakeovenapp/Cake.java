2c343f782f68999978fff65349ebe2441cf949b6
Enny457 <osgonganenkhzaya@gmail.com>
2025-04-16 12:53:54 +0100

    Update Cake.java
    Added comments to explain Cake.java class and its methods

--- cakeovenapp/Cake.java
+++ cakeovenapp/Cake.java
@@ -4,20 +4,21 @@ import java.time.LocalDateTime;
 /**
  *
  * @author ennyy
- */
+// Represents a Cake with name, weight, best before date, and time it was added
 public class Cake {
     private String name;
     private int weight;
     private LocalDate bestBefore;
     private LocalDateTime addedTime;
-
+// Constructor to intialize a new cake object
     public Cake(String name, int weight, LocalDate bestBefore) {
         this.name = name;
         this.weight = weight;
         this.bestBefore = bestBefore;
         this.addedTime = LocalDateTime.now(); 
+// Auto set the time when the cakes added
     }
-
+//Getters
     public String getName() {
         return name;
     }
@@ -33,7 +34,7 @@ public class Cake {
     public LocalDateTime getAddedTime() {
         return addedTime;
     }
-
+// Added toString for displaying the cake nicelyyy
     @Override
     public String toString() {
         return name + " (" + weight + "g, Best Before: " + bestBefore + ", Added: " + addedTime + ")";
